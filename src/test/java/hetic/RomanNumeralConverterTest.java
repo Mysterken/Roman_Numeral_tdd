af9a06164907e576ce3de2a3b40fa179d193ebcd
Mysterken <Mysterkenzo@gmail.com>
2024-06-27 16:05:49 +0200

    Add some more random tests

--- src/test/java/hetic/RomanNumeralConverterTest.java
+++ src/test/java/hetic/RomanNumeralConverterTest.java
@@ -21,4 +21,34 @@ public class RomanNumeralConverterTest {
     public void testSeven() {
         assertEquals("VII", converter.convertToRoman(7));
     }
+
+    @Test
+    public void testFifty() {
+        assertEquals("L", converter.convertToRoman(50));
+    }
+
+    @Test
+    public void testFour() {
+        assertEquals("IV", converter.convertToRoman(4));
+    }
+
+    @Test
+    public void testNine() {
+        assertEquals("IX", converter.convertToRoman(9));
+    }
+
+    @Test
+    public void testTwenty() {
+        assertEquals("XX", converter.convertToRoman(20));
+    }
+
+    @Test
+    public void testThirty() {
+        assertEquals("XXX", converter.convertToRoman(30));
+    }
+
+    @Test
+    public void testForty() {
+        assertEquals("XL", converter.convertToRoman(40));
+    }
 }
