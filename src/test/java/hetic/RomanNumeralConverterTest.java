f6f4c94b9f8ced514e2241829a3a043b12a5a787
Mysterken <Mysterkenzo@gmail.com>
2024-06-27 15:45:02 +0200

    Fill junit test

--- src/test/java/hetic/RomanNumeralConverterTest.java
+++ src/test/java/hetic/RomanNumeralConverterTest.java
@@ -1,13 +1,24 @@
 package hetic;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralConverterTest {
 
+    // todo : get converter class
+
+    @Test
     public void testOne() {
+        assertEquals("I", converter.convertToRoman(1));
     }
 
+    @Test
     public void testTen() {
+        assertEquals("X", converter.convertToRoman(10));
     }
 
+    @Test
     public void testSeven() {
+        assertEquals("VII", converter.convertToRoman(7));
     }
 }
